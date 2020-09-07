85809c551fdd3f88409e88cc7d3312890893f046
Venny <sgtmattis@gmail.com>
2020-09-08 01:13:26 +0200

    after of this commit remember do a git reset hard

--- ReptilianBot/lib/constants/blacklist.ts
+++ ReptilianBot/lib/constants/blacklist.ts
@@ -1,1 +1,1 @@
-export const blacklist = ['nigger', 'n1gg3r', 'pokimane fart compilation 4k dolby surround'];
+export const blacklist = ['nigger', 'kike', 'pokimane fart compilation 4k dolby surround'];
