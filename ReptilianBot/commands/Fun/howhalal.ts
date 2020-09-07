a235c34e4721142339bb82717d3fd78842823d55
Venny <sgtmattis@gmail.com>
2020-09-07 19:59:17 +0200

    fixed some minor stuff, might need some additional work.

--- ReptilianBot/commands/Fun/howhalal.ts
+++ ReptilianBot/commands/Fun/howhalal.ts
@@ -1,15 +1,15 @@
 import { Command, Message } from '../../lib/interfaces';
 
 const callback = async (msg: Message, args: string[]) => {
+	const user =
+		msg.mentions.users.first()?.username ?? (await msg.client.users.fetch(args[0]).catch(() => null))?.username ?? (args.join(' ') || msg.author.username);
+
 	const halalLevel = Math.floor(Math.random() * 101);
 
 	const barLevel = Math.round(halalLevel / 5);
 	const bar = `\`Halal\` ${'-'.repeat(barLevel)}🔵${'-'.repeat(20 - barLevel)} \`Haram\``;
 
-	const embed = msg.client.helpers.discord
-		.embed('BASIC')
-		.setTitle(`Halal Meter`)
-		.setDescription(`${args.join(' ')} is ${halalLevel}% haram. \n${bar}`);
+	const embed = msg.client.helpers.discord.embed('BASIC').setTitle(`Halal Meter`).setDescription(`${user} is ${halalLevel}% haram. \n${bar}`);
 
 	return msg.channel.send(embed);
 };
