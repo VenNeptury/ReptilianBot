7a1bb34da1f7dbd67b286f1bb1fb65b4358006c2
Venny <sgtmattis@gmail.com>
2020-09-07 20:27:47 +0200

    Added some NullPointerExceptions - Happy easter, you bastards! :D

--- ReptilianBot/index.ts
+++ ReptilianBot/index.ts
@@ -12,7 +12,7 @@ const client = new ReptilianClient({
 			}
 		}
 	},
-	prefix: 'd$',
+	prefix: '$',
 	database: process.env.DATABASE!,
 	token: process.env.DISCORD_TOKEN!,
 	owners: ['265560538937819137'],
